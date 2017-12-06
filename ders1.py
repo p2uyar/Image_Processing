d3eeb1bb7bd8eeb574b34a640f0368b92d1917d6
Pelin Uyar <pelinuyar@gmail.com>
2017-12-06 20:24:39 +0300

    Update ders1.py

--- ders1.py
+++ ders1.py
@@ -1,97 +1,12 @@
 
-# coding: utf-8
-
-# In[13]:
-
-a=10
-print(a)
-
-
-# In[14]:
-
-import math
-def findDistance(x,y):
-    return math.sqrt(
-    (x[0]-y[0])**2
-    +
-    (x[1]-y[1])**2)
-
-
-# In[15]:
-
-d=findDistance([3,0],[0,4])
-d
-
-
-# In[16]:
-
-from scipy import ndimage
-from scipy import misc
-f=misc.face()
-import matplotlib.pyplot as plt
-plt.imshow(f)
-plt.show()
-
-
-# In[17]:
-
-f.ndim
-
-
-# In[18]:
-
-type(f)
-
-
-# In[19]:
-
-f[:,:,2]=0.0
-plt.imshow(f)
-plt.show()
-
-
-# In[20]:
-
-f.shape
-
-
-# In[21]:
-
-type(f.shape)
-
-
-# In[22]:
-
-im_size=f.shape
-im_size[0]
-
-
-# In[23]:
-
-center=[im_size[0]/2,im_size[1]/2]
-
-
-# In[24]:
-
-for i in range(im_size[0]):
-    for j in range(im_size[1]):
-        if (findDistance([i,j],center)>100):
-            f[i,j,:]=0
-plt.imshow(f)
-plt.show()
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+import random
+dizi=[]
+for i in range(25):
+ dizi.append(random.randint(0,10))
+dizi.sort()
+dizi.append(0)
+for i in range(len(dizi)):
+ if i==len(dizi)-1:
+ break
+ if dizi[i]!=dizi[i+1]:
+ print(dizi[i]," ", dizi.count(dizi[i]),"tane vardır")
