ef3b0b05bd824bbdf2976372aef76f5bef971753
Pelin Uyar <pelinuyar@gmail.com>
2017-12-06 21:18:02 +0300

    Add files via upload

---
+++ Ders8.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+myNumbers = np.random.randint(0,2,10)
+myHistogram = {}
+myNumbers # siyah beyaz resim olacak
+for number in myNumbers:
+    if number in myHistogram.keys():
+        myHistogram[number]=myHistogram[number]+1
+    else:
+        myHistogram[number]=1
+myNumbers, myHistogram
+# histogram da iki değer olmalı
+# bunların oranı yazı-matbaa çıktısı için % kaçtır
+
